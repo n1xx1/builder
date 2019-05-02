19b8d4a89d9cf9d42cd51fc8eb42d9b886557395
Simone Miraglia <simone@wearezam.com>
2019-05-02 17:14:13 +0200

    implement new cond: generic comparison operator

---
+++ cond_op.go
@@ -0,0 +1,98 @@
+// Copyright 2016 The Xorm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package builder
+
+import (
+	"fmt"
+)
+
+type Operator int
+
+const (
+	OpEq Operator = iota
+	OpNeq
+)
+
+type op struct {
+	op Operator
+	a  interface{}
+	b  interface{}
+}
+
+func (o Operator) String() string {
+	switch o {
+	case OpEq:
+		return "="
+	case OpNeq:
+		return "<>"
+	default:
+		return ""
+	}
+}
+
+// Operator generate a comparison operator SQL
+func Op(o Operator, a interface{}, b interface{}) Cond {
+	return op{o, a, b}
+}
+
+func writeTerm(v interface{}, w Writer) error {
+	switch v.(type) {
+	case Cond:
+		if _, err := fmt.Fprint(w, "("); err != nil {
+			return err
+		}
+		if err := v.(Cond).WriteTo(w); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprint(w, ")"); err != nil {
+			return err
+		}
+	case *Builder:
+		if _, err := fmt.Fprint(w, "("); err != nil {
+			return err
+		}
+		if err := v.(*Builder).WriteTo(w); err != nil {
+			return err
+		}
+		if _, err := fmt.Fprint(w, ")"); err != nil {
+			return err
+		}
+	default:
+		if _, err := fmt.Fprint(w, "?"); err != nil {
+			return err
+		}
+		w.Append(v)
+	}
+	return nil
+}
+
+// WriteTo writes SQL to Writer
+func (o op) WriteTo(w Writer) error {
+	if err := writeTerm(o.a, w); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprint(w, o.op.String()); err != nil {
+		return err
+	}
+	if err := writeTerm(o.b, w); err != nil {
+		return err
+	}
+	return nil
+}
+
+// And implements And with other conditions
+func (o op) And(conds ...Cond) Cond {
+	return And(o, And(conds...))
+}
+
+// Or implements Or with other conditions
+func (o op) Or(conds ...Cond) Cond {
+	return Or(o, Or(conds...))
+}
+
+// IsValid tests if this op is valid
+func (o op) IsValid() bool {
+	return true
+}
